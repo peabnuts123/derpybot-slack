4c74076023ad834214e5d7e3dd77c3890f2f7fd7
peabnuts123 <peabnuts123@gmail.com>
2017-02-23 22:28:30 +1300

    Updating Cleverbot Library

--- test.js
+++ test.js
@@ -4,41 +4,46 @@ var argv = require('yargs').argv;
 
 var cleverbotInstance = new Cleverbot();
 
-Cleverbot.prepare(function () {
-	var currentToken = argv.token;
+var currentToken = argv.token;
+var cleverbotApiKey = argv['cleverbot-api-token'];
 
-    if (toString.call(currentToken) !== '[object String]') {
-        throw new Error("No valid token presented. Please use --token to specify a token");
-    }
 
-    var controller = Botkit.slackbot({
-        debug: false
-    });
+if (toString.call(currentToken) !== '[object String]') {
+	throw new Error("No valid token present. Please use --token to specify a token");
+}
+if (toString.call(cleverbotApiKey) !== '[object String]') {
+	throw new Error("No valid cleverbot API key present. Please use --cleverbot-api-token to specify a token");
+}
 
-    var bot = controller.spawn({
-        token: currentToken
-    }).startRTM();
+cleverbotInstance.configure({botapi: cleverbotApiKey});
 
-    controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function (bot, message) {
-        bot.reply(message, "Shutting down...");
-        setTimeout(function () {
-            process.exit();
-        }, 3000);
-    });
+var controller = Botkit.slackbot({
+	debug: false
+});
 
-    controller.hears(['.*'], 'direct_message,direct_mention,mention', function (bot, slackMessage) {
-		bot.api.users.info({token: currentToken, user: slackMessage.user}, function(error, response) {
-			if(!error) {
-				slackMessage.text = slackMessage.text.replace(/^@derpy\s*/, "").replace(/derpybot/ig, "CleverBot").replace(/(?:Derpy|@derpy)/g, "CleverBot")
+var bot = controller.spawn({
+	token: currentToken
+}).startRTM();
 
-				cleverbotInstance.write(slackMessage.text, function (cbResponse) {
-					cbResponse.message.replace(/cleverbot/ig, "Derpy");
+controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function (bot, message) {
+	bot.reply(message, "Shutting down...");
+	setTimeout(function () {
+		process.exit();
+	}, 3000);
+});
 
-					bot.reply(slackMessage, response.user.name + ": " + cbResponse.message);
-				});			
-			} else {
-				bot.reply("ERROR OCCURRED :(");
-			}
-		});
-    });
+controller.hears(['.*'], 'direct_message,direct_mention,mention', function (bot, slackMessage) {
+	bot.api.users.info({token: currentToken, user: slackMessage.user}, function(error, response) {
+		if(!error) {
+			slackMessage.text = slackMessage.text.replace(/^@derpy\s*/, "").replace(/derpybot/ig, "CleverBot").replace(/(?:Derpy|@derpy)/g, "CleverBot")
+
+			cleverbotInstance.write(slackMessage.text, function (cbResponse) {
+				cbResponse.output.replace(/cleverbot/ig, "Derpy");
+
+				bot.reply(slackMessage, response.user.name + ": " + cbResponse.output);
+			});			
+		} else {
+			bot.reply("ERROR OCCURRED :(");
+		}
+	});
 });
